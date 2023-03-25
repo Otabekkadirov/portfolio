45ea982f1e33cdd26f0a0c34c34fd01c9b28dbd6
Otabekkadirov <ganijanovich94@gmail.com>
2023-03-25 17:37:24 +0500

    portfolio tugatildi

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener("keydown", playSound);
+
+function playSound(e) {
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+    if (!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add("playing");
+
+    setTimeout(() => {
+        key.classList.remove("playing");
+    }, 100);
+}
